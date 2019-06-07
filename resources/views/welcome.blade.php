9639d91bc9c0c4963110d79bf4d87234b86c4227
camillemcooper <48187933+camillemcooper@users.noreply.github.com>
2019-06-07 10:25:22 -0400

    fixed controller problem

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,13 +5,4 @@
 
 <h1>Yummy Food Restaurant</h1>
 
-<<<<<<< HEAD
-=======
-<ul>
-@foreach($tasks as $task)
-<li>{{$task}}</li>
-@endforeach
-</ul>
-
->>>>>>> 69237768a4559c913bd8b3c79f0f630496fef06f
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -11,23 +11,9 @@
 |
 */
 
-<<<<<<< HEAD
 Route::get('/', 'PagesController@home');
 Route::get('/contact', 'PagesController@contact');
 Route::get('/about', 'PagesController@about');
-=======
-Route::get('/', function () {
-    $tasks = [
-        'Go to the store',
-        'Go to the market',
-        'Go to work',
-        'Go to the concert',
-    ];
-    return view('welcome',[
-        'tasks' => $tasks,
-        'foo' => 'happy']);
-});
->>>>>>> 69237768a4559c913bd8b3c79f0f630496fef06f
 
 Auth::routes();
 
